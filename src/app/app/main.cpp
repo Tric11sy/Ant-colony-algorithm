171f82819f25166098579c920b7ac555ab441d87
Kirill <tricksy2020@mail.ru>
2023-05-10 13:16:08 +0700

    First step for Ant algorithm

--- src/app/app/main.cpp
+++ src/app/app/main.cpp
@@ -4,9 +4,15 @@ int main() {
     // Debug
     setbuf(stdout, nullptr);
 
-    auto random_graph = ant::random_graph(10, 100);
+    // ant::AntColony();
 
-    random_graph.print();
+    //Создание графа
+    auto graph = ant::random_graph(10, 100);
+
+    graph.print();
+    std::cout << std::endl;
+
+    graph.change_phero(1, 9);
 
     return 0;
 }
--- src/libcolony/libcolony/ant.hpp
+++ src/libcolony/libcolony/ant.hpp
@@ -3,9 +3,27 @@
 #include <list>
 #include <map>
 
+#include <algorithm>
+#include <vector>
+
 //Для использования псевдо-рандома
 #include <cstdlib>
 #include <ctime>
+
+//Количество муравьев
+constexpr int ant_count = 10;
+
+//Параметры для алгоритма
+
+//Скорость испарения
+constexpr double RHO = 0.5;
+
+//Обновение феромона
+double update_phero(const double old_phero) {
+    double new_phero = 1.0 + old_phero;
+
+    return new_phero;
+}
 
 namespace ant {
 
@@ -14,13 +32,18 @@ void test_func();
 //Ребро
 struct Edge {
     //Конструктор инициализации
-    Edge(int dest, int len) : dest_edge(dest), length(len){};
+    Edge(int dest, int len, double new_phero)
+        : dest_edge(dest), length(len), phero(new_phero){};
 
+    //Вторая вершина
     int dest_edge;
+    //Длина ребра
     int length;
+    //Феромоны
+    double phero;
 };
 
-//Граф
+//Граф(TO DO: Добавить метод изменения феромонов)
 class Graph {
    public:
     //Конструктор по умолчанию
@@ -29,9 +52,9 @@ class Graph {
     //Добавление вершины в граф
     void add_edge(int src, int dest, int cost) {
         //Добавление ребра dest - src
-        list_map[src].push_back(Edge(dest, cost));
+        list_map[src].push_back(Edge(dest, cost, 1.0 / edge_count));
         //Добавление ребра src - dest
-        list_map[dest].push_back(Edge(src, cost));
+        list_map[dest].push_back(Edge(src, cost, 1.0 / edge_count));
     }
 
     //Печать графа
@@ -47,6 +70,22 @@ class Graph {
             }
             std::cout << std::endl;
         }
+    }
+
+    //Обновление феромонов(вершины для определения нужного ребра)
+    void change_phero(int src, int dest) {
+        std::cout << "Src:   " << src << std::endl;
+        std::cout << "Dest:   " << dest << std::endl;
+
+        // src - индекс в карте, dest - вершина в списке
+
+        //Поиск нужной вершины
+        auto iter = std::find_if(
+            list_map[src].begin(),
+            list_map[src].end(),
+            [&](const Edge& edge) -> bool { return edge.dest_edge == dest; });
+
+        std::cout << "  " << (*iter).dest_edge << ":" << (*iter).length;
     }
 
    private:
@@ -105,6 +144,86 @@ Graph random_graph(const int vertices, const int desnsity) {
         }
     }
     return new_graph;
+}
+
+//"Муравей"
+class Ant {
+   public:
+    //Конструктор инициализации
+    Ant(int new_current) {
+        current = new_current;
+        path_length = -1;
+    }
+
+    //Вывод списка табу
+    void print_tabu() {
+        for (auto item : tabu) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    //Вывод пути
+    void print_path() {
+        for (auto item : path) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    //Добавление вершины в путь
+    void add_path(int new_elem) { path.push_back(new_elem); }
+
+    //Добавление вершины в список табу
+    void add_tabu(int new_tabu) { tabu.push_back(new_tabu); }
+
+   private:
+    //Текущая вершина
+    int current;
+    //Список табу
+    std::vector<int> tabu;
+    //Пусть
+    std::vector<int> path;
+    //Длина пути
+    int path_length;
+};
+
+//Муравьиный алгоритм
+void AntColony() {
+    //Создание графа
+    auto graph = random_graph(30, 50);
+
+    graph.print();
+
+    //Инициализация муравьев
+    std::vector<Ant> ants;
+    for (int index = 0; index < ant_count; index++) {
+        //Создание муравья(по одному в вершине)???
+        //Важно: добавить проверку для размера графа
+        Ant new_ant(index);
+
+        //Добавление текущей вершины в путь
+        new_ant.add_path(index);
+        //Добавление текущей вершины в список табу
+        new_ant.add_tabu(index);
+
+        //Добавление в вектор муравьев
+        ants.push_back(new_ant);
+    }
+
+    /*
+            //Вывод списка табу
+            for(auto item : ants)
+            {
+                item.print_tabu();
+            }
+
+            //Вывод пути
+            for(auto item : ants)
+            {
+                item.print_path();
+            }
+    */
 }
 
 }  // namespace ant
